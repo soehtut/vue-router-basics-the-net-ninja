5808f56efba36eef4f42b472a2109532d8629473
SoeHtut <soehtut@escapepixel.com>
2021-01-17 13:07:52 +0630

    Router Links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet quaerat ad expedita nulla ea architecto veniam sed error libero animi enim sunt, natus exercitationem quae aspernatur perferendis distinctio molestias possimus.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
