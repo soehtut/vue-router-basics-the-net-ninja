286ca9171f4af7ed52e094f4327f334151b7eb28
SoeHtut <soehtut@escapepixel.com>
2021-01-17 13:19:02 +0630

    Folder Structure

---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id">{{ job.title }}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+                {id: 1, title: 'Web Designer'},
+                {id: 2, title: 'Web Developer'},
+                {id: 3, title: 'React Native Developer'}
+            ]
+        }
+    }
+}
+</script>
+
