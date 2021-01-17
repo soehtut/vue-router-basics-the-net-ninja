390033d647239a493e5e03eb1967439d6baeaf13
SoeHtut <soehtut@escapepixel.com>
2021-01-17 13:41:47 +0630

    Route Parameters

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Job Details</h1>
+    <!-- <div>Job ID is {{ $route.params.id }}</div> -->
+    <div>Job ID is {{ id }}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    }
+}
+</script>
